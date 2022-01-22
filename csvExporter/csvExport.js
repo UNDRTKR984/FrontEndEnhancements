afc14ae46ed65751a83a600a3f93cf70fb308ccf
Matt Vogel <63422957+UNDRTKR984@users.noreply.github.com>
2022-01-21 21:45:34 -0500

    CSV Exporter on the front end using javascript
    To use just have an add click with csvExport({name of the JSON object});

---
+++ csvExporter/csvExport.js
@@ -0,0 +1,24 @@
+function csvExport(data){
+
+    var fields = Object.keys(data[0])
+    var replacer = function(key, value) { return value === null ? '' : value } 
+    var csv = data.map(function(row){
+      return fields.map(function(fieldName){
+        return JSON.stringify(row[fieldName], replacer)
+      }).join(',')
+    })
+    csv.unshift(fields.join(',')) // add header column
+    csv = csv.join('\r\n');
+
+    var downloadLink = document.createElement("a");
+    var blob = new Blob(["\ufeff", csv]);
+    var url = URL.createObjectURL(blob);
+    downloadLink.href = url;
+    downloadLink.download = "data.csv";
+
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+    
+    return (console.log(csv));
+}
